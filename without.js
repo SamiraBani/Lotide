8f2a251ae806fcc2ceede834d8675ac1ba19aef0
SamiraBani <banihassan.s@gmail.com>
2021-01-07 00:42:36 +0000

    getting middle elements of an array

--- without.js
+++ without.js
@@ -1,27 +1,27 @@
 
-// const eqArrays = function(array1,array2) {
-//    if (array1.length !== array2.length) {
-//      return false;
-//    }
-//    for (let i = 0; i < array1.length; i++) {
-//      if (array1[i] !== array2[i]) {
-//        return false;
-//      }
-//    }
-//    return true;
-//  };
+const eqArrays = function(array1,array2) {
+   if (array1.length !== array2.length) {
+     return false;
+   }
+   for (let i = 0; i < array1.length; i++) {
+     if (array1[i] !== array2[i]) {
+       return false;
+     }
+   }
+   return true;
+ };
 
 
-// const assertArrayEqual = function(ar1,ar2){
-//    const comparison = eqArrays(ar1,ar2)
+const assertArrayEqual = function(ar1,ar2){
+   const comparison = eqArrays(ar1,ar2)
 
-//    if(comparison){
-//       return console.log(`✅ Assertion is passed: "${ar1}" === "${ar2}"`);
-//    }
-//    else{
-//       return console.log(`🛑 Assertion is failed: "${ar1}" !== "${ar2}"`);
-//    }
-// };
+   if(comparison){
+      return console.log(`✅ Assertion is passed: "${ar1}" === "${ar2}"`);
+   }
+   else{
+      return console.log(`🛑 Assertion is failed: "${ar1}" !== "${ar2}"`);
+   }
+};
 
 // assertArrayEqual([1, 2, 3], [1, 2, 3]);
 
@@ -39,4 +39,8 @@ const without = function(array,itemsToRemove){
    }
    return myArray;
 }
-console.log(without([1,2,3,4,5],[3,4]));
+
+let word = [1,2,3,4,5];
+console.log(without(word,[3,4]));
+console.log(assertArrayEqual(word,[1,2,3,4,5]));
+console.log(without(['a','b','c','d'],['c']));
